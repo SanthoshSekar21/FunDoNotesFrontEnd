3ce079d27a27c71a5e7b3fe371d06190b583363d
santhosh <santhosh20sekar@gmail.com>
2024-11-27 11:06:19 +0000

    [santhosh] added: navigator to notes container

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, Sanitizer } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { ARCHIVE_ICON, TRASH_ICON } from 'src/assets/svg-icons';
+import { ARCHIVE_ICON, NOTE_ICON, TRASH_ICON } from 'src/assets/svg-icons';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,6 +14,7 @@ drawerState:boolean=false
 constructor( private iconRegistry: MatIconRegistry,private sanitizer: DomSanitizer, private router:Router){
   iconRegistry.addSvgIconLiteral('archive-icon', sanitizer.bypassSecurityTrustHtml(ARCHIVE_ICON));
   iconRegistry.addSvgIconLiteral('trash-icon', sanitizer.bypassSecurityTrustHtml(TRASH_ICON));
+  iconRegistry.addSvgIconLiteral('notes-icon',sanitizer.bypassSecurityTrustHtml(NOTE_ICON ))
 }
 handleDrawerClick(){
   this.drawerState=!this.drawerState;
@@ -29,5 +30,8 @@ navigateToArchive(){
 }
 navigateToTrash(){
   this.router.navigate(['dashboard/trash'])
+}
+navigateToNotes(){
+  this.router.navigate(['dashboard/notes'])
 }
 }
