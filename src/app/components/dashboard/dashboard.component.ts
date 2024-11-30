286eacbef58d6b2d47c3a231502d7585622a3ce6
santhosh <santhosh20sekar@gmail.com>
2024-11-30 22:31:25 +0530

    [santhosh] updated: dashboard header

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { ARCHIVE_ICON, NOTE_ICON, TRASH_ICON } from 'src/assets/svg-icons';
+import { ARCHIVE_ICON, LIST_VIEW_ICON, NOTE_ICON, REFRESH_ICON, SETTING_ICON, TRASH_ICON } from 'src/assets/svg-icons';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,7 +12,7 @@ import { ARCHIVE_ICON, NOTE_ICON, TRASH_ICON } from 'src/assets/svg-icons';
 export class DashboardComponent {
   isHalfOpen = true; // Start in the half-open state
   isExpanded = false; // Start in the half-open state, not fully expanded
-
+  selectedItem: string = 'notes';
  
   constructor(
     public iconRegistry:MatIconRegistry,
@@ -22,18 +22,12 @@ export class DashboardComponent {
     iconRegistry.addSvgIconLiteral('archive-icon', sanitizer.bypassSecurityTrustHtml(ARCHIVE_ICON));
     iconRegistry.addSvgIconLiteral('trash-icon', sanitizer.bypassSecurityTrustHtml(TRASH_ICON));
     iconRegistry.addSvgIconLiteral('notes-icon', sanitizer.bypassSecurityTrustHtml(NOTE_ICON));
+    iconRegistry.addSvgIconLiteral('setting-icon',sanitizer.bypassSecurityTrustHtml(SETTING_ICON))
+    iconRegistry.addSvgIconLiteral('list-view-icon',sanitizer.bypassSecurityTrustHtml(LIST_VIEW_ICON))
   }
 
   toggleDrawer() {
-    if (this.isHalfOpen) {
-      // If half-open, expand it fully
-      this.isHalfOpen = false;
-      this.isExpanded = true;
-    } else if (this.isExpanded) {
-      // If fully expanded, close it halfway
-      this.isExpanded = false;
-      this.isHalfOpen = true;
-    }
+  this.isExpanded=!this.isExpanded;
   }
 
   stopPropagation(event: Event) {
@@ -41,14 +35,17 @@ export class DashboardComponent {
   }
 
   navigateToArchive() {
+    this.selectedItem = 'archive';
     this.router.navigate(['dashboard/archive']);
   }
 
   navigateToTrash() {
+    this.selectedItem = 'trash';
     this.router.navigate(['dashboard/trash']);
   }
 
   navigateToNotes() {
+    this.selectedItem = 'notes';
     this.router.navigate(['dashboard/notes']);
   }
 }
