9060960dbf7fc4f799dc1814f5001b1a8800c46a
santhosh <santhosh20sekar@gmail.com>
2024-12-05 22:34:23 +0530

    [santhosh] added: edit-label functionality

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
 import { DataService } from 'src/app/service/dataservices/data.service';
 import { ARCHIVE_ICON, EDIT_ICON, LIST_VIEW_ICON, NOTE_ICON, REFRESH_ICON, REMINDER_ICON, SETTING_ICON, TRASH_ICON } from 'src/assets/svg-icons';
+import { EditLabelComponent } from '../edit-label/edit-label.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +13,7 @@ import { ARCHIVE_ICON, EDIT_ICON, LIST_VIEW_ICON, NOTE_ICON, REFRESH_ICON, REMIN
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  isHalfOpen = true; 
+  labels: string[] = [];
   isExpanded = false; 
   selectedItem: string = 'notes';
   isDrawerOpen = false;
@@ -19,7 +21,7 @@ export class DashboardComponent {
     public iconRegistry:MatIconRegistry,
     public sanitizer:DomSanitizer,
     private router: Router,
-    private data: DataService
+    private data: DataService,private dialog: MatDialog
   ) {
     iconRegistry.addSvgIconLiteral('remainder-icon',sanitizer.bypassSecurityTrustHtml(REMINDER_ICON));
     iconRegistry.addSvgIconLiteral('archive-icon', sanitizer.bypassSecurityTrustHtml(ARCHIVE_ICON));
@@ -27,7 +29,7 @@ export class DashboardComponent {
     iconRegistry.addSvgIconLiteral('notes-icon', sanitizer.bypassSecurityTrustHtml(NOTE_ICON));
     iconRegistry.addSvgIconLiteral('setting-icon',sanitizer.bypassSecurityTrustHtml(SETTING_ICON));
     iconRegistry.addSvgIconLiteral('list-view-icon',sanitizer.bypassSecurityTrustHtml(LIST_VIEW_ICON));
-    iconRegistry.addSvgIconLiteral('edit-icon',sanitizer.bypassSecurityTrustHtml(EDIT_ICON))
+    iconRegistry.addSvgIconLiteral('edit-icon',sanitizer.bypassSecurityTrustHtml(EDIT_ICON));
   }
    
   ngOnInit(): void {
@@ -65,4 +67,13 @@ export class DashboardComponent {
     localStorage.removeItem('token');
     this.router.navigate(['']);
   }
+  openEditLabelDialog() {
+    const dialogRef = this.dialog.open(EditLabelComponent, {
+      width: '400px',
+      data: { labels: this.labels }
+    });
 }
+
+}
+
+
--- src/app/components/edit-label/edit-label.component.ts
+++ src/app/components/edit-label/edit-label.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edit-label',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-label.component.scss']
 })
 export class EditLabelComponent {
+  labels: string[] = [];
+  newLabel: string = '';
 
+  constructor(
+    public dialogRef: MatDialogRef<EditLabelComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { labels: string[] }
+  ) {
+    this.labels = data.labels;
+  }
+
+  createLabel() {
+    const trimmedLabel = this.newLabel.trim();
+    if (trimmedLabel && !this.labels.includes(trimmedLabel)) {
+      this.labels.push(trimmedLabel);
+    }
+    this.newLabel = '';
+  }
+
+  renameLabel(index: number) {
+    const newLabel = this.labels[index];
+
+    if (newLabel && !this.labels.includes(newLabel)) {
+      this.labels[index] = newLabel;
+    }
+  }
+
+  deleteLabel(index: number) {
+    this.labels.splice(index, 1);
+  }
+  cancelCreation(){
+    this.dialogRef.close();
+  }
 }
