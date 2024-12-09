a12fc5cccd39e861ab74dfd7eeebb37d2514b4a7
santhosh <santhosh20sekar@gmail.com>
2024-12-09 17:26:48 +0530

    [santhosh]added: testimg for the data Service

--- src/app/service/dataservices/data.service.spec.ts
+++ src/app/service/dataservices/data.service.spec.ts
@@ -1,16 +1,32 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { DataService } from './data.service';
 
-// import { DataService } from './data.service';
+describe('DataService', () => {
+  let service: DataService;
 
-// describe('DataService', () => {
-//   let service: DataService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(DataService);
+  });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(DataService);
-//   });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should have an initial value for incomingData as an empty string', (done: DoneFn) => {
+    service.incomingData.subscribe((value) => {
+      expect(value).toBe(''); // The default value of BehaviorSubject
+      done();
+    });
+  });
+
+  it('should update incomingData when outgoingData is called', (done: DoneFn) => {
+    const testMessage = 'Test Message';
+    service.outgoingData(testMessage); // Updating the BehaviorSubject value
+
+    service.incomingData.subscribe((value) => {
+      expect(value).toBe(testMessage); // Expect updated value
+      done();
+    });
+  });
+});
